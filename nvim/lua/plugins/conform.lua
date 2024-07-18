8052b20b5365188de3c847041c67cdbedf4ce399
Rachel Aalders <raalders@boomerweb.nl>
2024-07-18 09:58:27 +0200

    formatter changes

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,28 +1,29 @@
 return {
-	'stevearc/conform.nvim',
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				-- Conform will run multiple formatters sequentially
-				python = { "isort", "black" },
-				-- Use a sub-list to run only the first available formatter
-				javascript = { { "prettierd", "prettier" } },
-				typescript = { { "prettierd", "prettier" } },
-			},
-		})
+    'stevearc/conform.nvim',
+    config = function()
+        require('conform').setup {
+            formatters_by_ft = {
+                lua = { 'stylua' },
+                -- Conform will run multiple formatters sequentially
+                python = { 'isort', 'black' },
+                -- Use a sub-list to run only the first available formatter
+                javascript = { { 'prettierd', 'prettier' } },
+                typescript = { { 'prettierd', 'prettier' } },
+            },
+        }
 
-		-- Create a command `:Format` local to the LSP buffer
-		vim.api.nvim_create_user_command('Format', function(args)
-			local range = nil
-			if args.count ~= -1 then
-				local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-				range = {
-					start = { args.line1, 0 },
-					["end"] = { args.line2, end_line:len() },
-				}
-			end
-			require("conform").format({ async = true, lsp_fallback = true, range = range })
-		end, { desc = 'Format current buffer with LSP' })
-	end
+        vim.keymap.set('', '<leader>f', function()
+            require('conform').format { async = true, lsp_fallback = true }
+        end, { desc = '[F]ormat buffer' })
+
+        -- Create a command `:Format` local to the LSP buffer
+        vim.api.nvim_create_user_command('Format', function(args)
+            local range = nil
+            if args.count ~= -1 then
+                local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+                range = { start = { args.line1, 0 }, ['end'] = { args.line2, end_line:len() } }
+            end
+            require('conform').format { async = true, lsp_fallback = true, range = range }
+        end, { desc = 'Format current buffer with LSP' })
+    end,
 }
