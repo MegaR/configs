ca3e1d594ccb45426c5e8d641397e8806b7cbf65
Rachel Aalders <raalders@boomerweb.nl>
2024-07-23 09:20:20 +0200

    light and darkmode switch

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,5 +1,8 @@
 return {
-    'Shatur/neovim-ayu',
+    'f-person/auto-dark-mode.nvim',
+    dependencies = {
+        'Shatur/neovim-ayu',
+    },
     config = function()
         require('ayu').setup {
             mirage = true,
@@ -15,6 +18,17 @@ return {
                 VertSplit = { bg = 'None' },
             },
         }
+
         vim.cmd 'colorscheme ayu-mirage'
+        local auto_dark_mode = require('auto-dark-mode')
+        auto_dark_mode.setup {
+            update_interval = 1000,
+            set_dark_mode = function()
+                vim.cmd 'colorscheme ayu-mirage'
+            end,
+            set_light_mode = function()
+                vim.cmd 'colorscheme ayu-light'
+            end,
+        }
     end,
 }
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -13,43 +13,87 @@ config.font = wezterm.font 'Monaspace Neon'
 
 config.harfbuzz_features = { "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08", "ss09", "calt", "dlig", "liga" }
 
+function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
+
 -- For example, changing the color scheme:
 -- config.color_scheme = 'Catppuccin Mocha'
 -- config.color_scheme = 'Gruvbox Dark (Gogh)'
 -- config.color_scheme = 'nord'
-config.color_scheme = 'Ayu Mirage'
+if get_appearance() == 'Dark' then
+  config.color_scheme = 'Ayu Mirage'
 
-config.colors = {
-  tab_bar = {
-    background = '#1F2430',
-    active_tab = {
-      bg_color = '#33415E',
-      fg_color = '#D8DEE9',
-    },
-    inactive_tab = {
-      bg_color = '#1F2430',
-      fg_color = '#D8DEE9',
-    },
-    inactive_tab_hover = {
-      bg_color = '#232834',
-      fg_color = '#D8DEE9',
-    },
-    new_tab = {
-      bg_color = '#1F2430',
-      fg_color = '#D8DEE9',
-    },
-    new_tab_hover = {
-      bg_color = '#232834',
-      fg_color = '#D8DEE9',
-    },
+  config.colors = {
+    tab_bar = {
+      background = '#1F2430',
+      active_tab = {
+        bg_color = '#607080',
+        fg_color = '#D8DEE9',
+      },
+      inactive_tab = {
+        bg_color = '#1F2430',
+        fg_color = '#D8DEE9',
+      },
+      inactive_tab_hover = {
+        bg_color = '#232834',
+        fg_color = '#D8DEE9',
+      },
+      new_tab = {
+        bg_color = '#1F2430',
+        fg_color = '#D8DEE9',
+      },
+      new_tab_hover = {
+        bg_color = '#232834',
+        fg_color = '#D8DEE9',
+      },
+    }
   }
-}
 
-config.window_background_gradient = {
-  colors = { '#1F2430', '#323A4C' },
-  -- Specifices a Linear gradient starting in the top left corner.
-  orientation = { Linear = { angle = -45.0 } },
-}
+  config.window_background_gradient = {
+    colors = { '#1F2430', '#323A4C' },
+    -- Specifices a Linear gradient starting in the top left corner.
+    orientation = { Linear = { angle = -45.0 } },
+  }
+
+else
+  config.color_scheme = 'ayu_light'
+
+  config.colors = {
+    tab_bar = {
+      background = '#FAFAFA',
+      active_tab = {
+        bg_color = '#F2AE49',
+        fg_color = '#575F66',
+      },
+      inactive_tab = {
+        bg_color = '#FAFAFA',
+        fg_color = '#575F66',
+      },
+      inactive_tab_hover = {
+        bg_color = '#E6E7E9',
+        fg_color = '#575F66',
+      },
+      new_tab = {
+        bg_color = '#FAFAFA',
+        fg_color = '#575F66',
+      },
+      new_tab_hover = {
+        bg_color = '#E6E7E9',
+        fg_color = '#575F66',
+      },
+    }
+  }
+
+  config.window_background_gradient = {
+    colors = { '#FAFAFA', '#E6E7E9' },
+    -- Specifices a Linear gradient starting in the top left corner.
+    orientation = { Linear = { angle = -45.0 } },
+  }
+end
 
 config.window_background_opacity = 0.99
 config.macos_window_background_blur = 10
