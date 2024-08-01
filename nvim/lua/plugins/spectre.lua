e100fe999aa6c2b6ba84545b01d5a9c0f073501b
Rachel Aalders <raalders@boomerweb.nl>
2024-08-01 15:06:30 +0200

    spectre trouble integration

--- nvim/lua/plugins/spectre.lua
+++ nvim/lua/plugins/spectre.lua
@@ -2,6 +2,9 @@ return {
     'nvim-pack/nvim-spectre',
     opts = {},
     config = function()
+        require('spectre').setup({
+            use_trouble_qf = true,
+        })
         vim.keymap.set('n', '<leader>S', require("spectre").toggle, {
             desc = 'Toggle Spectre',
         })
