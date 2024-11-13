e232708f868c35973692a829805c674cb165ce6c
Rachel <raalders28@gmail.com>
2024-11-13 12:28:37 +0100

    lualine tweaks

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,14 +1,23 @@
 return {
     'nvim-lualine/lualine.nvim',
+    dependencies = {
+        { 'yavorski/lualine-macro-recording.nvim' },
+        { 'yavorski/lualine-lsp-client-name.nvim' },
+    },
     opts = {
         options = {
             icons_enabled = true,
-            component_separators = '|',
+            -- component_separators = '|',
+            component_separators = '',
             section_separators = '',
             theme = 'kanagawa',
         },
         extensions = {
             'oil',
+        },
+        sections = {
+            lualine_c = { 'filename', 'macro_recording', '%S' },
+            lualine_x = { 'lsp_client_name', 'encoding', 'fileformat', 'filetype' },
         },
     },
 }
