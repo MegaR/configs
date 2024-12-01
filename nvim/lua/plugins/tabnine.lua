89103e64ff3735274b251f3037008e3e8a03a416
Rachel <raalders28@gmail.com>
2024-12-01 11:28:48 +0100

    disable tabnine

--- nvim/lua/plugins/tabnine.lua
+++ nvim/lua/plugins/tabnine.lua
@@ -1,14 +1,15 @@
-return {
-    'codota/tabnine-nvim',
-    build = './dl_binaries.sh',
-    config = function()
-        require('tabnine').setup {
-            disable_auto_comment = true,
-            accept_keymap = '<C-j>',
-            dismiss_keymap = '<C-e>',
-            debounce_ms = 800,
-            exclude_filetypes = { 'TelescopePrompt', 'neo-tree' },
-            log_file_path = nil,
-        }
-    end,
-}
+return {}
+-- return {
+--     'codota/tabnine-nvim',
+--     build = './dl_binaries.sh',
+--     config = function()
+--         require('tabnine').setup {
+--             disable_auto_comment = true,
+--             accept_keymap = '<C-j>',
+--             dismiss_keymap = '<C-e>',
+--             debounce_ms = 800,
+--             exclude_filetypes = { 'TelescopePrompt', 'neo-tree' },
+--             log_file_path = nil,
+--         }
+--     end,
+-- }
