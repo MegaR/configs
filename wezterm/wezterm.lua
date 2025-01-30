d7d1b9ed1eb0784442aa021f960fb403789d7b1e
Rachel Aalders <rachel@Rachels-MacBook-Air.local>
2025-01-30 11:57:01 +0100

    wezterm padding

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -70,10 +70,10 @@ end
 
 config.window_decorations = 'RESIZE'
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+  left = 6,
+  right = 6,
+  top = 6,
+  bottom = 6,
 }
 
 local function is_vim(pane)
