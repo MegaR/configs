e933b4c0e82288b62d96baaecb9aaba7a306f3f2
Rachel Aalders <rachel@Rachels-MacBook-Air.local>
2025-02-21 13:53:18 +0100

    formatting fixes

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -7,14 +7,16 @@ return {
                 -- Conform will run multiple formatters sequentially
                 python = { 'isort', 'black' },
                 -- Use a sub-list to run only the first available formatter
-                javascript = { 'prettierd', 'prettier', stop_after_first = true },
-                typescript = { 'prettierd', 'prettier', stop_after_first = true },
+                javascript = { 'prettier' },
+                javascriptreact = { 'prettier' },
+                typescript = { 'prettier' },
+                typescriptreact = { 'prettier' },
                 php = { 'pint' },
             },
         }
 
         vim.keymap.set('', '<leader>f', function()
-            require('conform').format { async = true, lsp_fallback = true }
+            require('conform').format { async = true, lsp_fallback = 'fallback' }
         end, { desc = '[F]ormat buffer' })
 
         -- Create a command `:Format` local to the LSP buffer
@@ -24,7 +26,7 @@ return {
                 local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
                 range = { start = { args.line1, 0 }, ['end'] = { args.line2, end_line:len() } }
             end
-            require('conform').format { async = true, lsp_fallback = true, range = range }
+            require('conform').format { async = true, lsp_fallback = 'fallback', range = range }
         end, { desc = 'Format current buffer with LSP' })
 
         vim.api.nvim_create_autocmd('BufWritePre', {
--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -148,7 +148,23 @@ return {
             css_variables = {},
             jsonls = {},
             sqlls = {},
-            yamlls = {},
+            yamlls = {
+                schemas = {
+                    kubernetes = '*.yaml',
+                    ['http://json.schemastore.org/github-workflow'] = '.github/workflows/*',
+                    ['http://json.schemastore.org/github-action'] = '.github/action.{yml,yaml}',
+                    ['http://json.schemastore.org/ansible-stable-2.9'] = 'roles/tasks/*.{yml,yaml}',
+                    ['http://json.schemastore.org/prettierrc'] = '.prettierrc.{yml,yaml}',
+                    ['http://json.schemastore.org/kustomization'] = 'kustomization.{yml,yaml}',
+                    ['http://json.schemastore.org/ansible-playbook'] = '*play*.{yml,yaml}',
+                    ['http://json.schemastore.org/chart'] = 'Chart.{yml,yaml}',
+                    ['https://json.schemastore.org/dependabot-v2'] = '.github/dependabot.{yml,yaml}',
+                    ['https://json.schemastore.org/gitlab-ci'] = '*gitlab-ci*.{yml,yaml}',
+                    ['https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json'] = '*api*.{yml,yaml}',
+                    ['https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json'] = '*docker-compose*.{yml,yaml}',
+                    ['https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json'] = '*flow*.{yml,yaml}',
+                },
+            },
             -- phpactor = {},
             intelephense = {},
             tailwindcss = {},
@@ -223,7 +239,7 @@ return {
             widgets.centered_float(widgets.scopes)
         end, { desc = 'Scopes' })
         vim.keymap.set('n', '<Leader>dt', function()
-            require'telescope'.extensions.dap.commands{}
+            require('telescope').extensions.dap.commands {}
         end, { desc = 'Scopes' })
 
         -- You can add other tools here that you want Mason to install
@@ -234,7 +250,7 @@ return {
             'eslint_d',
             'luacheck',
             'markdownlint',
-            'prettierd',
+            'prettier',
             'stylua',
             'pint',
             'php-debug-adapter',
