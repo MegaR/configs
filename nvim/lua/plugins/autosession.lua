52fd42b14a81290e37fda4017ecb421466e1be44
Rachel <raalders28@gmail.com>
2025-09-08 10:37:44 +0200

    updates autosession

--- nvim/lua/plugins/autosession.lua
+++ nvim/lua/plugins/autosession.lua
@@ -1,19 +1,19 @@
 return {
-	'rmagatti/auto-session',
-	-- lazy = true,
-	config = function()
-		require("auto-session").setup {
-			log_level = "error",
-			pre_save_cmds = { require("neo-tree.sources.manager").close_all },
-			bypass_session_save_file_types = { "neo-tree", "spectre" },
-			auto_save_enabled = true,
-			auto_restore_enabled = true,
-			auto_session_use_git_branch = true,
-			cwd_change_handling = {
-				post_cwd_changed_hook = function ()
-					require('lualine').refresh()
-				end
-			},
-		}
-	end,
+    'rmagatti/auto-session',
+    -- lazy = true,
+    config = function()
+        require('auto-session').setup {
+            log_level = 'error',
+            pre_save_cmds = { require('neo-tree.sources.manager').close_all },
+            bypass_save_filetypes = { 'neo-tree', 'spectre' },
+            auto_save = true,
+            auto_restore = true,
+            git_use_branch_name = true,
+            cwd_change_handling = {
+                post_cwd_changed_hook = function()
+                    require('lualine').refresh()
+                end,
+            },
+        }
+    end,
 }
