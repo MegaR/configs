a6cc8e591da4f37c9b72a6791d1e475880727c4b
Rachel Aalders <rachel.aalders@zintouch.nl>
2025-12-08 09:05:53 +0100

    todo comments tweaks

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -1,7 +1,23 @@
 return {
     'folke/todo-comments.nvim',
     dependencies = { 'nvim-lua/plenary.nvim', 'folke/snacks.nvim' },
-    opts = {},
+    opts = {
+        keywords = {
+            FIX = { icon = ' ', color = 'error', alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE', '\\!' } },
+            TODO = { icon = ' ', color = 'info', alt = { '\\*' } },
+            HACK = { icon = ' ', color = 'warning' },
+            WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
+            PERF = { icon = ' ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
+            NOTE = { icon = ' ', color = 'hint', alt = { 'INFO', '\\?' } },
+            TEST = { icon = '⏲ ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
+        },
+        highlight = {
+            pattern = [[.*<(KEYWORDS):?\s*]],
+        },
+        search = {
+            pattern = [[\b(KEYWORDS)\b]],
+        },
+    },
     keys = {
         {
             '<leader>st',
@@ -21,3 +37,15 @@ return {
         },
     },
 }
+
+-- TODO test
+-- FIX test
+-- HACK test
+-- WARN test
+-- test TODO dkfjdf
+-- INFO test
+-- TODOtest
+-- * test
+-- ? test
+-- ! test
+-- AA test
