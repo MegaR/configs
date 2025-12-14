f20fd90e14e8aa987bff55e1a4a115a0706539aa
Rachel <raalders28@gmail.com>
2025-12-14 21:37:30 +0100

    oil git status

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,13 +1,16 @@
 return {
-	'stevearc/oil.nvim',
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		local oil = require('oil')
-		oil.setup({
-			view_options = {
-				show_hidden = true,
-			},
-		})
-		vim.keymap.set("n", "<leader>o", oil.open, { desc = "Oil" })
-	end,
+    'stevearc/oil.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+        local oil = require 'oil'
+        oil.setup {
+            view_options = {
+                show_hidden = true,
+            },
+            win_options = {
+                signcolumn = 'yes:2',
+            },
+        }
+        vim.keymap.set('n', '<leader>o', oil.open, { desc = 'Oil' })
+    end,
 }
